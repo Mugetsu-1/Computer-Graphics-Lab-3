0673134b44cd555e1375256903c7e94fd1a08551
Saugat Thapa <128715197+Mugetsu-1@users.noreply.github.com>
2025-03-09 19:17:34 +0545

    Update no.2(Clip).cpp

--- Computer Graphics Lab-3/no.2(Clip).cpp
+++ Computer Graphics Lab-3/no.2(Clip).cpp
@@ -4,10 +4,10 @@
 
 using namespace std;
 
-#define LEFT 1       // 1 in binary is 0001
-#define RIGHT 2      // 2 in binary is 0010
-#define BOTTOM 4     // 4 in binary is 0100
-#define TOP 8        // 8 in binary is 1000
+#define LEFT 1
+#define RIGHT 2
+#define BOTTOM 4
+#define TOP 8
 
 const int xmin = 100, ymin = 100, xmax = 300, ymax = 300;
 
@@ -20,9 +20,19 @@ int getRegionCode(int x, int y) {
     return regionCode;
 }
 
+void applyMatrixTransformation(float matrix[2][2], int &x, int &y) {
+    int newX = matrix[0][0] * x + matrix[0][1] * y;
+    int newY = matrix[1][0] * x + matrix[1][1] * y;
+    x = newX;
+    y = newY;
+}
+
 void cohenSutherlandClip(int &x1, int &y1, int &x2, int &y2) {
-    int regionCode1 = getRegionCode(x1, y1), regionCode2 = getRegionCode(x2, y2);
+    int regionCode1 = getRegionCode(x1, y1);
+    int regionCode2 = getRegionCode(x2, y2);
     bool accept = false;
+
+    float identityMatrix[2][2] = {{1, 0}, {0, 1}};
 
     while (true) {
         if ((regionCode1 | regionCode2) == 0) {
@@ -34,29 +44,30 @@ void cohenSutherlandClip(int &x1, int &y1, int &x2, int &y2) {
             int outsideRegionCode = regionCode1 ? regionCode1 : regionCode2;
             int x, y;
 
-            if (outsideRegionCode & TOP) { 
-                x = x1 + (x2 - x1) * (ymax - y1) / (y2 - y1); 
-                y = ymax; 
-            } else if (outsideRegionCode & BOTTOM) { 
-                x = x1 + (x2 - x1) * (ymin - y1) / (y2 - y1); 
-                y = ymin; 
-            } else if (outsideRegionCode & RIGHT) { 
-                y = y1 + (y2 - y1) * (xmax - x1) / (x2 - x1); 
-                x = xmax; 
-            } else { 
-                y = y1 + (y2 - y1) * (xmin - x1) / (x2 - x1); 
-                x = xmin; 
+            if (outsideRegionCode & TOP) {
+                x = x1 + (x2 - x1) * (ymax - y1) / (y2 - y1);
+                y = ymax;
+            } else if (outsideRegionCode & BOTTOM) {
+                x = x1 + (x2 - x1) * (ymin - y1) / (y2 - y1);
+                y = ymin;
+            } else if (outsideRegionCode & RIGHT) {
+                y = y1 + (y2 - y1) * (xmax - x1) / (x2 - x1);
+                x = xmax;
+            } else {
+                y = y1 + (y2 - y1) * (xmin - x1) / (x2 - x1);
+                x = xmin;
             }
 
-            // Update the endpoint outside the rectangle
-            if (outsideRegionCode == regionCode1) { 
-                x1 = x; 
-                y1 = y; 
-                regionCode1 = getRegionCode(x1, y1); 
-            } else { 
-                x2 = x; 
-                y2 = y; 
-                regionCode2 = getRegionCode(x2, y2); 
+            applyMatrixTransformation(identityMatrix, x, y);
+
+            if (outsideRegionCode == regionCode1) {
+                x1 = x;
+                y1 = y;
+                regionCode1 = getRegionCode(x1, y1);
+            } else {
+                x2 = x;
+                y2 = y;
+                regionCode2 = getRegionCode(x2, y2);
             }
         }
     }
@@ -67,22 +78,17 @@ int main() {
     int gd = DETECT, gm;
     initgraph(&gd, &gm, (char*) "");
 
-    // Draw the clipping rectangle
     rectangle(xmin, ymin, xmax, ymax);
 
-    // Get the coordinates for the line segment from the user
     int x1, y1, x2, y2;
     cout << "Enter coordinates of the line (x1, y1, x2, y2): ";
     cin >> x1 >> y1 >> x2 >> y2;
 
-    // Draw the original line
     line(x1, y1, x2, y2);
 
-    // Clip the line using Cohen-Sutherland algorithm
     cohenSutherlandClip(x1, y1, x2, y2);
 
     getch();
     closegraph();
     return 0;
 }
-
